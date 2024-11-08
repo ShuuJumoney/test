0f891c0535bf3ccc2e9531b5e0c8c986ded85eac
ShuuJumoney <asshu03@gmail.com>
2024-11-08 10:49:00 +0900

    Add files via upload

--- js/jumoney.js
+++ js/jumoney.js
@@ -1418,7 +1418,7 @@ document.addEventListener("DOMContentLoaded", function () {
     	const cacheKey = `${npc}_${server}_${channel}`; // 중복 호출을 피하기 위한 캐시키 생성 //호출 횟수 아껴야함...ㅠㅠ
         let url = `https://open.api.nexon.com/mabinogi/v1/npcshop/list?npc_name=${npc}&server_name=${server}&channel=${channel}`;
         if(SHARE_KEY) url = `https://shuuryn.com/nexon_api5.php?npc=${npc}&server=${server}&channel=${channel}`;
-
+		console.log(url);
     	//리셋 시간되면 무조건 캐시 초기화 밑 tables 초기화
     	if(isResetNeeded()){
 			document.getElementById("tables").innerHTML = "";
@@ -1435,7 +1435,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		
 	  		// 호출 진행 중임을 기록
 	 		inProgressCalls.add(cacheKey);
-	        console.log(url);
+	        
 			//간혹 리셋 되었는데 캐시된 데이터 사용한다는 로그가 뜨며 리스트 생성 안하는 증상 방지
 			if( dataCache[cacheKey] ) { //데이터가 없다면 다시 불러오기
 	            console.log(`캐시된 데이터 사용: ${cacheKey}`);
@@ -1463,11 +1463,15 @@ document.addEventListener("DOMContentLoaded", function () {
 			if( !SHARE_KEY ) { 
             	response = await fetch(url, { headers: { "x-nxopen-api-key": API_KEY } });
             	data = await response.json();
+            	console.log("###");
+            	console.log(data);
             }else{
 				response = await fetch(url);
     			data = await response.json();
+    			console.log("---");
+    			console.log(data);
 			}
-			console.log(data);
+			
             if(API_KEY && data.error) response.ok = false;
             if (!response.ok || !data.shop) {
 				const errorName = data.error.name;
